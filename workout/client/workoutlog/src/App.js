dc6f7fcce80d19493a64e475577e5414c2681465
AlanJschultz <alanjschultz@gmail.com>
2019-12-16 20:32:38 -0500

    12162019 react class started

---
+++ workout/client/workoutlog/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Example from './components/example';
+import Image from './components/image';
+
+function App() {
+  return (    
+    <div className="App">
+      <Example></Example>
+      <Image />
+    </div>
+  );
+}
+
+export default App;
