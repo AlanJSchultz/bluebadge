759b6939f7a465bc84a0fa7ada4813cb8010af39
AlanJschultz <alanjschultz@gmail.com>
2019-12-18 22:22:20 -0500

    12182019 finished log controller

--- workout/server/controllers/logcontroller.js
+++ workout/server/controllers/logcontroller.js
@@ -37,25 +37,75 @@ router.post('/', function (req, res) {
 
 // gets all logs for an individual
 router.get('/', function (req, res) {
-    
+    var userid = req.user.id;
+    LogModel.findAll({
+        where: { owner: userid }
+    }).then (
+        function findAllSuccess(data) {
+            res.json(data)
+        }, function findAll(err) {
+            res.send(500, err.message);
+        }
+    );
 });
 
 
 // gets individual logs by id for an individual user
 router.get('/:id', function (req, res) {
-    
+    var primaryKey = req.params.id;
+    var userid = req.user.id;
+    LogModel.findOne({
+        where: {
+            id: primaryKey,
+            owner: userid
+        }
+    }).then(
+        data => {
+            return res.json(data);
+        }),
+        err => res.send(500, err.message);
 });
 
 
 // allows individual logs to be updated by a user
 router.put('/:id', function (req, res) {
-    
+    var owner = req.user.id;
+    var primaryKey = req.params.id;
+    var description = req.body.log.description;
+    var definition = req.body.log.definition;
+    var result = req.body.log.result;
+
+    LogModel.update({
+        description: description,
+        definition: definition,
+        result: result,
+        owner: owner
+    },
+    {where:{id: primaryKey, owner: owner}}
+    ).then(
+        data => {
+            return res.json(data);
+        }),
+        err => res.send(500, err.message);
 });
 
 
 // allows individual logs to be deleted by a user
 router.delete('/:id', function (req, res) {
-    
+    var primaryKey = req.params.id;
+    var userid = req.user.id;
+
+    LogModel.destroy({
+        where:{
+            id: primaryKey,
+            owner: userid
+        }
+    }).then(data => {
+        return data > 0
+            ? res.send("Item was deleted")
+            : res.send("Nothing deleted");
+    }),
+        err => res.send(500, err.message);
 });
 
 
